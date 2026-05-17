75a4441e955198a0788710b9690354840221c566
AliSabet <ali30sabet114university@gmail.com>
2026-05-17 18:44:55 +0330

    Added properties for Account.java

--- src/MainClasses/Account.java
+++ src/MainClasses/Account.java
@@ -1,3 +1,54 @@
 package MainClasses;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+
+    private String password;
+
+    private String userName;
+
+    private List<Album> albums = new ArrayList<>();
+
+    private String id;
+
+    public Account(String password , String userName , String id){
+        this.password = password;
+        this.userName = userName;
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
 }
