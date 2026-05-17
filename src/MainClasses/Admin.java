14ee94810c8290a62ac12c12d26d735752c9a6ec
AliSabet <ali30sabet114university@gmail.com>
2026-05-17 18:59:34 +0330

    Added properties for Admin.java

--- src/MainClasses/Admin.java
+++ src/MainClasses/Admin.java
@@ -1,3 +1,39 @@
 package MainClasses;
 public class Admin {
+
+    private String adminName;
+
+    private String password;
+
+    private String id;
+
+    public Admin(String adminName , String password , String id){
+        this.adminName = adminName;
+        this.password = password;
+        this.id = id;
+    }
+
+    public String getAdminName(){
+        return adminName;
+    }
+
+    public void setAdminName(String adminName){
+        this.adminName = adminName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
 }
