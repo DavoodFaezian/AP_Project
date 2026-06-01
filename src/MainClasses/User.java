914a0393a5e9b79495dbe04ff22dfa7fbece82ff
AliSabet <ali30sabet114university@gmail.com>
2026-06-01 15:42:38 +0330

    User was compeleted

--- src/MainClasses/User.java
+++ src/MainClasses/User.java
@@ -1,6 +1,13 @@
 package MainClasses;
 
+import Exceptions.FieldIsEmptyException;
+import Exceptions.PasswordContainsUserNameException;
+import Exceptions.PasswordNotLongEnoughException;
+import Exceptions.PasswordNotStrongException;
+
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class User extends BaseClass<User>{
 
@@ -12,8 +19,45 @@ public class User extends BaseClass<User>{
 
     private Set<Album> albums = new TreeSet<>(Comparator.comparing(Album::getCreatedAt));
 
+    private static final int MIN_LENGTH = 8;
+
+    private void validateUserName(String userName){
+        if(userName.isEmpty()){
+            throw new FieldIsEmptyException("User name must not be empty!!!" , this.userName);
+        }
+    }
+
+    private void validatePassword(String password){
+        if(password.isEmpty()){
+            throw new FieldIsEmptyException("Password must not be empty!!!" , this.password);
+        }
+    }
+
+    private void validateLength(String password){
+        if(password.length() < MIN_LENGTH){
+            throw new PasswordNotLongEnoughException("Password must have at least 8 characters!!!");
+        }
+    }
+
+    private void validateStrength(String password){
+        if(!Pattern.compile("[!@#$%^&*+=_?]").matcher(password).find()){
+            throw new PasswordNotStrongException("Password must contain at least one special character!!!");
+        }
+    }
+
+    private void validateDoesNotContainUserName(String password){
+        if(password.contains(userName)){
+            throw new PasswordContainsUserNameException("Password must not contain user name!!!");
+        }
+    }
+
     public User(String userName, String password){
+        validateUserName(userName);
         this.userName = userName;
+        validatePassword(password);
+        validateLength(password);
+        validateStrength(password);
+        validateDoesNotContainUserName(password);
         this.password = password;
     }
 
@@ -30,10 +74,15 @@ public class User extends BaseClass<User>{
     }
 
     public void setUserName(String userName) {
+        validateUserName(userName);
         this.userName = userName;
     }
 
     public void setPassword(String password) {
+        validatePassword(password);
+        validateLength(password);
+        validateStrength(password);
+        validateDoesNotContainUserName(password);
         this.password = password;
     }
 
@@ -53,7 +102,7 @@ public class User extends BaseClass<User>{
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(getUserName(), user.getUserName());
+        return Objects.equals(getUserName(), user.getUserName()) && Objects.equals(getId() , user.getId());
     }
 
     @Override
