38cef521b056905d56e48e3cc3fd45684a020a73
AliSabet <ali30sabet114university@gmail.com>
2026-06-03 17:08:27 +0330

    UserTests was added to project

--- src/TestClasses/UserTests.java
+++ src/TestClasses/UserTests.java
@@ -1,22 +1,20 @@
+import Exceptions.FieldIsEmptyException;
+import Exceptions.PasswordContainsUserNameException;
+import Exceptions.PasswordNotLongEnoughException;
+import Exceptions.PasswordNotStrongException;
 import MainClasses.Theme;
 import MainClasses.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTests {
 
-    private User user1;
-    private User user2;
-
-    @BeforeEach
-    public void before(){
-        user1 = new User("" , "123456" , Theme.LIGHT);
-        user2 = new User("Davood" , "123456789*" , Theme.DARK);
-    }
-
     @Test
     public void passwordTest(){
-
+        assertThrows(FieldIsEmptyException.class , () -> {new User("Ali" , "" , Theme.LIGHT);});
+        assertThrows(PasswordNotLongEnoughException.class , () -> {new User("Davood" , "123" , Theme.LIGHT);});
+        assertThrows(PasswordNotStrongException.class , () -> {new User("Mahdi" , "123456789" , Theme.LIGHT);});
+        assertThrows(PasswordContainsUserNameException.class , () -> {new User("Amin" , "Amin1234567*" , Theme.LIGHT);});
+        assertDoesNotThrow(() -> {new User("Hamid" , "123456#45" , Theme.LIGHT);});
     }
 }
