ad433c16cab37dc8fc7c2f080ec9ba13eaa7e97a
AliSabet <ali30sabet114university@gmail.com>
2026-08-02 16:03:14 +0330

    ClientHandler and Server was added to project

---
+++ src/APIServer/ClientHandler.java
@@ -0,0 +1,49 @@
+package APIServer;
+
+import RequestHandler.RequestHandler;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ClientHandler implements Runnable {
+
+    private final Socket socket;
+
+    private final Gson gson = new Gson();
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())
+            )
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                line = line.trim();
+
+                Request request = gson.fromJson(line , Request.class);
+                RequestHandler handler = new RequestHandler(request);
+                Response response = handler.handle();
+
+                String responseString = gson.toJson(response);
+
+                writer.write(responseString);
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (SocketException e) {
+            System.out.println("Client disconnected");
+        } catch (IOException e) {
+            System.out.println("Error handling client");
+        }
+    }
+}
